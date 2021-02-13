f07f3c88f8804a8f90504a837eadc7e9fef6c525
milacerveira <milascerveira@gmail.com>
2021-02-13 09:38:11 +0000

    pilot

---
+++ src/main/java/Pilot.java
@@ -0,0 +1,35 @@
+public class Pilot {
+    private String name;
+    private String rank;
+    private String licenceNumber;
+
+    public Pilot(String name, String rank, String licenceNumber) {
+        this.name = name;
+        this.rank = rank;
+        this.licenceNumber = licenceNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public void setLicenceNumber(String licenceNumber) {
+        this.licenceNumber = licenceNumber;
+    }
+}
