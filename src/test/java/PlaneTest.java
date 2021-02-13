4771047643561f0de003a18f742ce7af94bfd564
milacerveira <milascerveira@gmail.com>
2021-02-13 10:04:03 +0000

    plane test capacity

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class PlaneTest {
+    Plane plane;
+
+
+    @Before
+    public void setUp() {
+        plane = new Plane(PlaneType.BOEING747);
+
+    }
+
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(100, plane.getCapacity());
+    }
+}
