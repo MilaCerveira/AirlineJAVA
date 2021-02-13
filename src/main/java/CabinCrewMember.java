fef4411e448cf35fcd49254fb841647f90c679ba
milacerveira <milascerveira@gmail.com>
2021-02-13 09:32:10 +0000

    cabin crew enum

--- src/main/java/CabinCrewMember.java
+++ src/main/java/CabinCrewMember.java
@@ -1,2 +1,25 @@
 public class CabinCrewMember {
+    private String name;
+    private CabinCrewMemberRank rank;
+
+    public CabinCrewMember(String name, CabinCrewMemberRank rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CabinCrewMemberRank getRank() {
+        return rank;
+    }
+
+    public void setRank(CabinCrewMemberRank rank) {
+        this.rank = rank;
+    }
 }
